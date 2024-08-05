f2110f52f79cc22b97550503e3cbb87a6d8f197e
mishra4567 <mishrapritam831@gmail.com>
2024-08-06 03:25:32 +0530

    Wordpress The Loop | Wordpress post Page

--- index.php
+++ index.php
@@ -8,10 +8,48 @@
 
  get_header();
 ?>
-<div class="content">
-    <?php esc_html_e('Content Index','dailyDoses'); ?>
-</div>
-    
+<!-- 06.08.2024 : 01.17 -->
+ <div class="" id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <?php if (have_posts()) {?>
+            <div class="container">
+                <?php if (is_home() &&  ! is_front_page()) { ?>
+                    <header class="mb-5">
+                        <h1 class="page-title "><?php single_post_title(); ?></h1>
+                    </header>
+                <?php } ?>
+               <div class="row">
+               <?php 
+               $index=0;
+               $no_of_columns=3;
+               //case index = 0;
+               // startthe loop
+               while(have_posts()): the_post();
+                    if (0===$index % $no_of_columns) {
+                ?>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                <?php } ?>
+                <h3><?php the_title() ?></h3>
+                <div class=""><?php the_excerpt() ?></div>
+                <?php 
+                    $index++;
+                    if(0 !== $index && 0 ===$index % $no_of_columns){
+                        ?>
+                </div>
+                        <?php
+                    }
+                endwhile;
+                ?>
+               </div>
+            </div>
+        <?php } ?>
+    </main>
+ </div>
+<!-- <div class="content">
+    <?php
+    //  esc_html_e('Content Index','dailyDoses'); 
+     ?>
+</div> -->
 <?php 
 get_footer();
 ?>
